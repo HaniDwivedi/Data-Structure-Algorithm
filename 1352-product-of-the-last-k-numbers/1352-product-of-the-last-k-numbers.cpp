b8a75d183712d776c98198a4eed2040abb06135e
Hani Dwivedi <dwivedihani900@gmail.com>
2026-07-31 10:38:03 +0530

    Time: 7 ms (99.73%), Space: 97.9 MB (98.02%) - LeetHub

--- 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.cpp
+++ 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.cpp
@@ -1,20 +1,24 @@
 class ProductOfNumbers {
 private:
-    vector<int> stream;
+    vector<int> prefix;
 
 public:
-    ProductOfNumbers() {}
+    ProductOfNumbers() {prefix.push_back(1);}
     
     void add(int num) {
-        stream.push_back(num);
+       if(num==0){
+        prefix.clear();
+        prefix.push_back(1);       
+    }else {
+        prefix.push_back(prefix.back()*num);
+    }
     }
     
     int getProduct(int k) {
-        int product = 1;
-        int n = stream.size();
-        for (int i = n - k; i < n; i++) {
-            product *= stream[i];
-        }
-        return product;
+     
+        int n = prefix.size();
+        if(k>=n)return 0;
+        return prefix[n-1]/prefix[n-k-1];
+       
     }
 };
