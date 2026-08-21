7cf8e5aaffe284aae68599c03a7c5ae1cfe47e0a
Hani Dwivedi <dwivedihani900@gmail.com>
2026-08-21 23:09:39 +0530

    Time: 0 ms (100%), Space: 8.9 MB (55.06%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string res = "";
+        int opened = 0;
+        
+        for (char c : s) {
+            if (c == '(') {
+                if (opened > 0) res += c;
+                opened++;
+            } else {
+                opened--;
+                if (opened > 0) res += c;
+            }
+        }
+        
+        return res;
+    }
+};
